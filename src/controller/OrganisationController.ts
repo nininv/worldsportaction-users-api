a3558cefa67474ffd7803197f4b7f4175de88914
nininv <nininv45@gmail.com>
2020-05-21 02:04:58 +0530

    OrganisationController.ts added
    (cherry picked from commit 6f6e3ea699f9bf249236fa2876d0b09e83731732)

---
+++ src/controller/OrganisationController.ts
@@ -0,0 +1,55 @@
+import {Get, JsonController, QueryParam, Authorized, HeaderParam, Res} from 'routing-controllers';
+import {BaseController} from "./BaseController";
+import { User } from '../models/User';
+import { Response, response } from 'express';
+import { logger } from '../logger';
+
+@JsonController('/api')
+export class OrganisationController extends BaseController {
+
+    @Authorized()
+    @Get('/organisation')
+    async organisation(
+        @QueryParam('userId') userId: number,
+        @HeaderParam("authorization") currentUser: User,
+        @Res() response: Response) {
+        try {
+            if (userId) {
+                if (userId && userId == currentUser.id) {
+
+                    const organisationRes = await this.organisationService.organisation();
+                    return response.status(200).send(organisationRes);
+
+                }
+            }
+        } catch (error) {
+            logger.error(`Error Occurred in organisation list ${userId}`+error);
+            return response.status(500).send({
+                message: 'Something went wrong. Please contact administrator'
+            });
+        }
+    }
+
+    @Authorized()
+    @Get('/userorganisation')
+    async userOrganisation(
+        @QueryParam('userId') userId: number,
+        @HeaderParam("authorization") currentUser: User,
+        @Res() response: Response) {
+        try {
+            if (userId) {
+                if (userId && userId == currentUser.id) {
+
+                    const organisationRes = await this.organisationService.userOrganisation(userId);
+                    return response.status(200).send(organisationRes);
+
+                }
+            }
+        } catch (error) {
+            logger.error(`Error Occurred in organisation list  ${userId}`+error);
+            return response.status(500).send({
+                message: 'Something went wrong. Please contact administrator'
+            });
+        }
+    }
+}
