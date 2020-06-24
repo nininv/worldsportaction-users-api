742c6f13788d658d2d911f1a484f08bacec8034d
nininv <nininv45@gmail.com>
2020-06-25 01:14:13 +0530

    CM-777

---
+++ src/constants/AppConstants.ts
@@ -0,0 +1,9 @@
+const AppConstants = {
+    loginErrMsg: "You don’t have access to the administration platform. Please contact your Affiliated Organisation to re-activate your access.",
+    loginUnsuccessfulMsg : "Login Unsuccessful. Incorrect Username or Password",
+    loginInvalidMsg: "Invalid username or password.",
+    sourceSystem: "WebAdmin",
+    bwsa: "BWSA"
+};
+
+export default AppConstants;
