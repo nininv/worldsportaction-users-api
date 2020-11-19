86345715faee5ef93d2a5475e32ca9589e954d6f
nininv <nininv45@gmail.com>
2020-11-20 01:22:13 +0530

    expiry date in user registering details

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -802,7 +802,7 @@ export default class UserService extends BaseService<User> {
                             // vouchers: item.vouchers,
                             //shopPurchases: item.shopPurchases,
                             paymentStatus: paymentStatus,
-                            expiryDate: item.expiryDate == null ? 'Single Use' : item.expiryDate,
+                            expiryDate: item.expiryDate ,
                             //paymentType: item.paymentType,
                             registrationForm: [],
                             alreadyDeRegistered: alreadyDeRegistered,
